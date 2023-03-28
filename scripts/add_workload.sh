97469c217941a4e497506fe2cefe876eb49e4588
Stefan Vasilev <stefan.vasilev@conclusion.nl>
2023-03-28 10:35:02 +0200

    test

--- scripts/add_workload.sh
+++ scripts/add_workload.sh
@@ -27,4 +27,4 @@ git config --global user.email "workload@conclusion.com"
 git config --global user.name "Workload user"
 git add .
 git commit -m "Added $new_workload"
-git push
+git push origin $new_branch_name
