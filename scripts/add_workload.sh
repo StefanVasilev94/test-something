7bb7b85d23aebc37a2019af63cee9cc64f1bc0ba
Stefan Vasilev <stefan.vasilev@conclusion.nl>
2023-03-28 11:23:50 +0200

    test

--- scripts/add_workload.sh
+++ scripts/add_workload.sh
@@ -6,7 +6,7 @@ repo_name=$2
 git_token=$3
 new_branch_name=$4
 
-git checkout -b $new_branch_name
+# git checkout -b $new_branch_name
 
 # Set the name of the YAML file
 yaml_file="workload.yml"
@@ -14,8 +14,8 @@ yaml_file="workload.yml"
 # Adds the new value to the workload array
 yq e ".jobs.terraform.strategy.matrix.workload += [\"$new_workload\"]" -i "$yaml_file"
 
-git config --global user.email "workload_bot@conclusion.com"
-git config --global user.name "Workload Bot"
-git add .
-git commit -m "Added $new_workload"
+# git config --global user.email "workload_bot@conclusion.com"
+# git config --global user.name "Workload Bot"
+# git add .
+# git commit -m "Added $new_workload"
 # git push origin $new_branch_name
