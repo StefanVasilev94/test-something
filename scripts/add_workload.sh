c467aaf364ba74fc0e65edd3b59adffe3cf31df9
Stefan Vasilev <stefan.vasilev@conclusion.nl>
2023-03-28 10:49:30 +0200

    test

--- scripts/add_workload.sh
+++ scripts/add_workload.sh
@@ -6,17 +6,6 @@ repo_name=$2
 git_token=$3
 new_branch_name=$4
 
-## clone repo
-git clone https://oauth2:$git_token@github.com/StefanVasilev94/$repo_name
-
-if [ ! -d "./$repo_name" ] 
-then
-    echo "Failed to retrieve $repo_name" 
-    exit 1 
-fi
-
-cd $repo_name
-
 git checkout -b $new_branch_name
 
 # Set the name of the YAML file
