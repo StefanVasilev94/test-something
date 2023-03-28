b0d90cdfcc8691e69e5adca1a4357cafec01e82a
Stefan Vasilev <stefan.vasilev@conclusion.nl>
2023-03-28 09:53:57 +0200

    test

--- scripts/add_workload.sh
+++ scripts/add_workload.sh
@@ -2,6 +2,19 @@
 
 # Set the new value to be added to the workload array
 new_workload=$1
+repo_name=$2
+git_token=$3
+
+## clone repo
+git clone https://oauth2:$git_token@github.com/StefanVasilev94/$repo_name
+
+if [ ! -d "./$repo_name" ] 
+then
+    echo "Failed to retrieve $repo_name" 
+    exit 1 
+fi
+
+cd $repo_name
 
 # Set the name of the YAML file
 yaml_file="workload.yml"
